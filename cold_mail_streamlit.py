35aa3ccb51a7eb9788769ef3007113ccce8183b5
Darshan Parsoliya <109534955+darshan220902@users.noreply.github.com>
2024-09-08 15:23:44 +0530

    Update cold_mail_streamlit.py

--- cold_mail_streamlit.py
+++ cold_mail_streamlit.py
@@ -5,8 +5,8 @@ from langchain_core.exceptions import OutputParserException
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_groq import ChatGroq
 #__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import chromadb
 import pandas as pd
 import uuid
