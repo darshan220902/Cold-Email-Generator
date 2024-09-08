4fce3cb36e020ad52a83f617c01b81e41f218f69
Darshan Parsoliya <109534955+darshan220902@users.noreply.github.com>
2024-09-08 15:22:24 +0530

    Update cold_mail_streamlit.py

--- cold_mail_streamlit.py
+++ cold_mail_streamlit.py
@@ -90,7 +90,7 @@ def email_generate(job, links, name, company, designation, api_key):
 
         1. Clearly highlight how {company_name} can address the client's needs as described in the job description.
         2. Emphasize the relevant skills and qualifications required for the job.
-        3. Incorporate the most pertinent links from {company_name}'s portfolio to showcase our capabilities and experience related to the job requirements.
+        3. Incorporate the most pertinent links from {link_list}'s portfolio to showcase our capabilities and experience related to the job requirements.
 
         Ensure that:
         - The email is directly tailored to the job description.
