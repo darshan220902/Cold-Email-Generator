59efc1e8621cfa09bd533d6ccd9cfce9bf8cda97
Darshan Parsoliya <109534955+darshan220902@users.noreply.github.com>
2024-11-03 16:09:38 +0530

    Update cold_mail_streamlit.py

--- cold_mail_streamlit.py
+++ cold_mail_streamlit.py
@@ -1,4 +1,4 @@
-    import streamlit as st
+import streamlit as st
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
